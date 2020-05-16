d7b294d67a893a01f659ac93e0db318545cea4c2
huanshenyi <txy1226052@gmail.com>
2020-05-16 18:05:14 +0900

    ログインform追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/login.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
   const routes = [
   {
@@ -11,14 +11,11 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/login')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/views/login.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="">
+        <div class="login-container">
+            <el-row>
+                <el-col :span="4" :offset="10">
+                    <h1 class="page-title">自動テストプラットフォーム</h1>
+                    <el-form ref="loginForm" :model="loginForm" label-width="80px" class="login-form" :rules="rules">
+                        <el-form-item label="email" prop="username">
+                            <el-input v-model="loginForm.username"></el-input>
+                        </el-form-item>
+                        <el-form-item label="password" prop="password">
+                            <el-input v-model="loginForm.password" type="password"></el-input>
+                        </el-form-item>
+                        <el-form-item >
+                            <el-button type="primary" @click="onsubmit">ログイン</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data() {
+            return {
+                loginForm: {
+                    username: "",
+                    password: ""
+                },
+                rules: {
+                    username: [
+                        {required: true, message: "アドレスを入れてください", trigger: "blur"},
+                    ],
+                    password: [
+                        {required: true, message: "パスワード入れてください", trigger: "blur"},
+                        {max: 100, message: "規定の長さを超えました", trigger: "blur"}
+                    ]
+                }
+            }
+        },
+        methods: {
+           onsubmit(){
+             this.$refs.loginForm.validate( valid => {
+                 if (!valid){
+                     return
+                 }
+                 const params = {
+                     username: this.loginForm.username,
+                     password: this.loginForm.password
+                 }
+             })
+           }
+        },
+        components: {}
+    }
+</script>
+
+<style lang="scss">
+.body{
+    background-color: #eee;
+}
+.login-container{
+    padding-top: 80px;
+    .page-title{
+        text-align: center;
+    }
+    .login-form{
+        padding-top: 40px;
+    }
+}
+</style>
